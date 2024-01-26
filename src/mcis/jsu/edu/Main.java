3eed32e8aa3ff6fa292714d6910177e9e65c0f70
Joel Cain <jcain1@stu.jsu.edu>
2024-01-25 20:43:06 -0600

    Initial Commit

---
+++ src/mcis/jsu/edu/Main.java
@@ -0,0 +1,8 @@
+
+package mcis.jsu.edu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TestClass.testMethod1());
+    }
+}
---
+++ src/mcis/jsu/edu/TestClass.java
@@ -0,0 +1,8 @@
+
+package mcis.jsu.edu;
+
+public class TestClass {
+    public static String testMethod1() {
+        return "Hello, World!";
+    }
+}
