c755a2f1325810678126648bf93b4fb6bbe1407c
Joel Cain <jcain1@stu.jsu.edu>
2024-01-27 19:04:21 -0600

    Added update comment on Main

--- src/edu/jsu/mcis/Main.java
+++ src/edu/jsu/mcis/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(TestClass.testMethod1());
     }
+    
+    /*
+    Cloned github repo into local repo. Pulled from remote. 
+    Added this comment so I could commit and then push to remote.
+    */
 }
